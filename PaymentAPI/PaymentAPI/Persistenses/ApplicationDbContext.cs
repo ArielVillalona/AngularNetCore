28af6c412249558cb3597baa87293bcae39c48b3
Ariel Villalona <avillalona@farmatrix.com.do>
2021-09-08 16:46:48 -0400

    Add project files.

---
+++ PaymentAPI/PaymentAPI/Persistenses/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaymentAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentAPI.Persistenses
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options){}
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
