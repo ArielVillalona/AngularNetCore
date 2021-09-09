8a7cc6ec780256701572dd689f574d58202dbb5d
ArielVillalona <a.villalona.reyes@hotmail.com>
2021-09-08 23:31:34 -0400

    form created

--- PaymentApp/src/app/app.module.ts
+++ PaymentApp/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -16,6 +17,7 @@ import { PaymentDetailsFormComponent } from './payment-details/payment-details-f
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     FontAwesomeModule
   ],
